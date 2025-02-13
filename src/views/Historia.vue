b0ae61697c38f1b96009826439d806893a884de6
Marcos Kaio <kaionery111@gmail.com>
2025-02-13 15:34:55 -0300

    implementando loading

--- src/views/Historia.vue
+++ src/views/Historia.vue
@@ -3,49 +3,37 @@
 
         <v-icon @click="voltar" style="font-size: 38px;">mdi-chevron-left</v-icon>
 
-        <div>
-            <div style="display: flex; justify-content: center;">
-                <v-card
-                    :image="livro.capa"
-                    width="90"
-                    height="130"
-                    class="mr-4"
-                ></v-card>
-                <div style="display: flex; flex-direction: column; justify-content: space-between;">
-                    <div>
-                        <h3 class="mb-1">{{livro.nome}}</h3>
-                        <div style="display: flex; gap: 5px;">
-                            <p v-for="(genero, index) in livro.generos" :key="index" class="tag">{{ genero.nome }}</p>
+        <div style="display: flex; align-items: center; justify-content: center;">
+            <v-progress-circular indeterminate v-if="isLoading"></v-progress-circular>
+
+            <div v-else>
+                <div style="display: flex; justify-content: center;">
+                    <v-card
+                        :image="livro.capa"
+                        width="90"
+                        height="130"
+                        class="mr-4"
+                    ></v-card>
+                    <div style="display: flex; flex-direction: column; justify-content: space-between;">
+                        <div>
+                            <h3 class="mb-1">{{livro.nome}}</h3>
+                            <div style="display: flex; gap: 5px;">
+                                <p v-for="(genero, index) in livro.generos" :key="index" class="tag">{{ genero.nome }}</p>
+                            </div>
+                        </div>
+                        <div style="display: flex; align-items: center;">
+                                <div class="avatar">M</div>
+                                <label class="avatar-titulo">{{livro.autor}}</label>
                         </div>
                     </div>
-                    <div style="display: flex; align-items: center;">
-                            <div class="avatar">M</div>
-                            <label class="avatar-titulo">{{livro.autor}}</label>
-                    </div>
                 </div>
-            </div>
-            <hr style="margin: 15px 30px 10px 30px">
-            <div style="padding: 0px 30px;">
-                <previa-acoes />
+                <hr style="margin: 15px 30px 10px 30px">
+                <div style="padding: 0px 30px;">
+                    <previa-acoes />
+                </div>
             </div>
         </div>
         
-
-        <!--
-        <div style="padding: 0px 20px 20px 20px; border-bottom: 1px solid #D9D9D9;">
-            <div style="display: flex; align-items: center; gap: 10px;">
-                <v-avatar color="pink-darken-1"> P </v-avatar>
-                <div style="display: flex; flex-flow: column;">
-                    <h3 class="mb-1">Um viciado nas estrelas</h3>
-                    <div style="display: flex; gap: 5px;">
-                        <p>Romance</p>
-                        <p>Aventura</p>
-                        <p>Mistério</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        -->
 
         <div style="padding: 20px; background-color: #F7F7F5;">
             <v-card v-for="(capitulo, index) in capitulos" :key="capitulo.id" class="mb-4 rounded-lg elevation-1"
@@ -78,14 +66,16 @@ export default {
     data() {
         return {
             livro: {},
-            capitulos: []
+            capitulos: [],
+            isLoading: true,
         };
     },
     methods: {
         voltar() {
             if (window.history.length > 1) {
                 this.$router.back();
-            } else {
+            } 
+            else {
                 this.$router.push("/");
             }
         },
@@ -109,6 +99,9 @@ export default {
         } 
         catch (error) {
             console.error("#ERRO AO BUSCAR LIVROS = ", error);
+        }
+        finally {
+            this.isLoading = false; // Garante que o loading será desativado sempre
         }
     },
 };
