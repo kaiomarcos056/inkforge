1a5969bf3c3adf7c9d52362e9ec4dc72dac390d5
Marcos Kaio <kaionery111@gmail.com>
2025-02-26 10:24:42 -0300

    comentarios

--- src/components/PreviaAcoes.vue
+++ src/components/PreviaAcoes.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="acoes">
         <div class="d-flex ga-1">
-            <img src="../assets/icons/comment.svg" alt="Ícone" width="24" />
+            <img src="../assets/icons/comment.svg" alt="Ícone" width="24" @click="abrircomentario()" />
             <p>20</p>
         </div>
         <div class="d-flex ga-1">
@@ -17,16 +17,62 @@
             <img src="../assets/icons/bookmark-selected.svg" alt="Ícone"  height="24" v-else @click="remover" />
             <img src="../assets/icons/share.svg" alt="Ícone" height="24" @click="compartilhar"/>
         </div>
+
+
+        <v-bottom-sheet v-model="bottomSheet" >
+            <v-card>
+                <div style="padding: 25px 15px; display: flex; flex-direction: column; gap: 10px; ">
+                    <div v-if="finalizando" style="display: flex; align-items: center; justify-content: center; margin: 30px;">
+                        <v-progress-circular indeterminate ></v-progress-circular>
+                    </div>
+                    <div style="flex-direction: column; min-height: 80vh; display: flex; gap: 10px;" v-else>
+                        <h3>Comentários</h3>
+
+                        <div style="flex: 1;">
+                            <div style="margin-bottom: 20px; display: flex; align-items: center; gap: 5px;" v-for="comentario in comentarios" :key="comentario.uuid_comentario">
+                                <v-avatar style="height: 40px; width: 40px; margin-right: 5px;" v-if="livro.foto !== ''">
+                                    <v-img :src="comentario.foto" ></v-img>
+                                </v-avatar>
+                                <div class="avatar" v-else>{{comentario.usuario.charAt(0)}}</div>
+                                <div>
+                                    <label for=""><b>{{comentario.usuario}}</b></label>
+                                    <p class="bottom-p">{{ comentario.comentario }}</p>
+                                </div>
+                            </div>
+                        </div>
+                        
+                        <div style="display: flex; align-items: center; gap: 10px;" v-if="!comentando">
+                            <v-avatar style="height: 40px; width: 40px; margin-right: 5px;" v-if="livro.foto !== ''">
+                                <v-img :src="auth.usuario.foto" ></v-img>
+                            </v-avatar>
+                            <div class="avatar" v-else>{{livro.autor.charAt(0)}}</div>
+                            <input type="text" placeholder="Adicione um comentário" v-model="comentario" style="flex: 1;" >
+                            <button @click="comentar" class="bottom-button" style="color: #fff; background-color: black;" :disabled="!comentario">
+                                <span class="mdi mdi-arrow-up"></span>
+                            </button>
+                        </div>
+                        <v-progress-linear indeterminate style="margin: 10px 0px;" v-else></v-progress-linear>
+                    </div>
+                </div>
+            </v-card>
+        </v-bottom-sheet>
+
     </div>
 </template>
 
 <script>
+import { authStore } from '@/stores/authStore';
 import axios from "axios";
 
 export default {
     name: "PreviaAcoes",
     data() {
         return {
+            bottomSheet: false,
+            finalizando: true,
+            comentario: '',
+            comentando: false,
+            comentarios: [],
         };
     },
     props: {
@@ -52,6 +98,43 @@ export default {
         },
     },
     methods: {
+        async comentar(){
+            try{
+                this.comentando = true;
+
+                const body = {
+                    uuid_usuario: this.uuid_usuario,
+                    uuid_livro: this.uuid_livro,
+                    comentario: this.comentario,
+                }
+
+                await axios.post(`https://inkforge-api.onrender.com/comentarios`, body, { headers: { "Content-Type": "application/json" } });
+            }
+            catch(e){
+
+            }
+            finally{
+                this.comentando = false;
+                this.comentario = '';
+                this.abrircomentario();
+            }
+        },
+
+        async abrircomentario(){
+            try{
+                this.bottomSheet = true
+                const comentarios = await axios.get(`https://inkforge-api.onrender.com/comentarios?uuid_livro=${this.uuid_livro}`);
+                this.comentarios = comentarios.data
+                console.log(this.comentarios)
+            }
+            catch(e){
+
+            }
+            finally{
+                this.finalizando = false
+            }
+        },
+
         async salvar(){
             //this.$emit('update:salvo', true);
             try{
@@ -114,6 +197,9 @@ export default {
             }
         },
     },
+    computed: {
+        auth() { return authStore().usuario }
+    }
 };
 </script>
 
@@ -135,5 +221,48 @@ export default {
 
 .icon:hover {
     color: #000;
+}
+
+.v-card.v-theme--light.v-card--density-default.v-card--variant-elevated {
+  border-radius: 25px 25px 0px 0px;
+}
+
+.bottom-button {
+    padding: 5px 10px;
+    border-radius: 57px;
+    font-size: 24px;
+}
+
+.bottom-button:disabled {
+    background-color: gray !important; /* Cor mais clara */
+    cursor: not-allowed; /* Indica que está desabilitado */
+    opacity: 0.5; /* Deixa o botão mais transparente */
+}
+
+input[type="text"] {
+    border: 2px solid #E2E2E2;
+    border-radius: 16px;
+    padding: 12px;
+    width: 100%;
+    font-size: 16px;
+    background-color: #fff;
+    transition: border-color 0.3s ease;
+}
+
+input[type="text"]:focus {
+    border-color: #151515; /* Muda a borda quando o input está focado */
+    outline: none; /* Remove o contorno padrão do navegador */
+}
+
+input[type="text"]:not(:placeholder-shown) {
+    border-color: #151515; /* Muda a borda quando o input tem algum valor */
+}
+
+p {
+    font-family: 'Noto Serif', serif;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 19.07px;
+    letter-spacing: -0.4px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,21 +1,16 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 
-// VUE ROUTER
 import router from "./router";
 
-// VUETIFY
 import { createVuetify } from 'vuetify';
 import 'vuetify/styles';
 import { aliases, mdi } from 'vuetify/iconsets/mdi';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 
-// PINIA
 import { createPinia } from 'pinia';
 import piniaPersist from 'pinia-plugin-persistedstate';
-
-import Vue3SocialSharingPlugin from "vue3-social-sharing";
 
 const pinia = createPinia();
 pinia.use(piniaPersist);
@@ -36,5 +31,4 @@ createApp(App)
   .use(vuetify)
   .use(router)
   .use(pinia)
-  .use(Vue3SocialSharingPlugin)
   .mount('#app');
